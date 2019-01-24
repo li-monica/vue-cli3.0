0e98f4372f5f7f87ebf0974a8f021426a659e7bf
liyunfeng <liyunfeng@jiangroom.com>
2019-01-24 15:55:51 +0800

    首次提交初始版

---
+++ src/pages/subpage/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img src="@/assets/logo.png">
+    <HelloWorld msg="多页面测试页"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    console.log('实时编译1213')
+    console.log(`这个是多页面测试页的主页：${this.$store.state.myname}`)
+  }
+}
+</script>
